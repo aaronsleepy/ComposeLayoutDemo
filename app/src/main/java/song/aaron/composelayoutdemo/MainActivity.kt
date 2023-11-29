955cf0a9eb9044c9abd82ff0d0f01b750f413040
Aaron Song <aaron@kmong.com>
2023-11-29 09:45:26 +0900

    ui: box layout 연습

--- app/src/main/java/song/aaron/composelayoutdemo/MainActivity.kt
+++ app/src/main/java/song/aaron/composelayoutdemo/MainActivity.kt
@@ -3,18 +3,22 @@ package song.aaron.composelayoutdemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -53,23 +57,44 @@ fun MainScreen() {
 }
 
 @Composable
-fun TextCell(text: String, modifier: Modifier = Modifier) {
+fun BoxLayoutMainScreen() {
+    Box(
+        contentAlignment = Alignment.CenterEnd,
+        modifier = Modifier.size(400.dp, 400.dp)
+            .clip(CutCornerShape(30.dp))
+            .background(Color.Blue)
+    ) {
+        val height = 200.dp
+        val width = 200.dp
+
+        TextCell(text = "1", Modifier.size(width = width, height=height).matchParentSize())
+        TextCell(text = "2", Modifier.size(width = width, height=height))
+        TextCell(text = "3", Modifier.size(width = width, height=height))
+    }
+}
+
+
+@Composable
+fun TextCell(text: String, modifier: Modifier = Modifier, fontSize: Int = 150) {
     val cellModifier = Modifier
         .padding(4.dp)
         .size(100.dp, 100.dp)
         .border(width = 4.dp, color = Color.Black)
 
-    Text(
-        text = text,
-        modifier = cellModifier.then(modifier),
-        fontSize = 70.sp,
-        fontWeight = FontWeight.Bold,
-        textAlign = TextAlign.Center
-    )
+    Surface {
+        Text(
+            text = text,
+            modifier = cellModifier.then(modifier),
+            fontSize = fontSize.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
+        )
+    }
 }
 
 @Composable
 @Preview(showBackground = true, showSystemUi = true)
 fun DefaultPreview() {
-    MainScreen()
+//    MainScreen()
+    BoxLayoutMainScreen()
 }
